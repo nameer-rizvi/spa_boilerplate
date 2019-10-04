5042c2631f777e724b84c0ece4c15dd8a83d485f
Nameer Rizvi <rizvinameer@gmail.com>
2019-10-04 17:14:24 -0400

    0 this one will work...

--- src/server/ssr.js
+++ src/server/ssr.js
@@ -4,11 +4,12 @@ const app_config = require("../config/index");
 const isProd = app_config["isProd"];
 const path = app_config["path"];
 
-const webpackDevMiddleware = require("webpack-dev-middleware");
-const webpack = require("webpack");
-const wp_config = require("../config/webpack/dev.js");
-const compiler = webpack(wp_config);
+function webpackDev() {
+  const webpackDevMiddleware = require("webpack-dev-middleware");
+  const webpack = require("webpack");
+  const wp_config = require("../config/webpack/dev.js");
+  const compiler = webpack(wp_config);
+  return webpackDevMiddleware(compiler, app_config["webpack"]);
+}
 
-module.exports = isProd
-  ? express.static(path.toDist())
-  : webpackDevMiddleware(compiler, app_config["webpack"]);
+module.exports = isProd ? express.static(path.toDist()) : webpackDev();
