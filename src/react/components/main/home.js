892ced79ceb4991aefa6226ce85fb3e2c6ccdfbf
Nameer Rizvi <rizvinameer@gmail.com>
2019-10-10 11:19:19 -0400

    Converted react packages to dev for lighter deployments; conditional messaging in react home page.

--- src/react/components/main/home.js
+++ src/react/components/main/home.js
@@ -1,10 +1,18 @@
 import React, { Fragment } from "react";
 
+const isProduction = require("../../../shared/index")["isProd"];
+
 const HomePage = () => {
-  return (
+  return isProduction ? (
+    <Fragment>
+      <h1>Well, would ya look at this?</h1>
+      <h2>The app is in production.</h2>
+    </Fragment>
+  ) : (
     <Fragment>
       <h1>hey, you got it running - nice!!</h1>
       <h2>now, let's make this thing :)</h2>
+      <h3>p.s. im at /src/react/components/main/home.js</h3>
     </Fragment>
   );
 };
--- src/webpack/html/index.js
+++ src/webpack/html/index.js
@@ -9,9 +9,7 @@ module.exports = isProd => {
     meta: {
       viewport: "width=device-width, initial-scale=1, shrink-to-fit=no",
       "theme-color": "#ffffff",
-      description: "Express app serving react via webpack.",
-      robots: "index,follow",
-      googlebot: "index,follow"
+      description: "Express app serving react via webpack."
     },
     minify: isProd,
     hash: false,
