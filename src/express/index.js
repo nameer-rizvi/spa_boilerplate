ba4160374fc74eb1173ee2e09c3e6dd1c00fb867
Nameer Rizvi <rizvinameer@gmail.com>
2020-03-07 21:46:51 -0500

    add json reader to express

--- src/express/index.js
+++ src/express/index.js
@@ -1,15 +1,17 @@
 require("dotenv").config();
 
 const express = require("express");
+
+express.json({ limit: "1mb" });
+
+const app = express();
+
 const cors = require("cors");
 const helmet = require("helmet");
 const historyApiFallback = require("connect-history-api-fallback");
 
-const app = express();
-
 app.use(cors());
 app.use(helmet());
-
 app.use(
   historyApiFallback({
     disableDotRule: true,
