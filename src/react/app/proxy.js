b9bf0a83809d5dd0c0dd8f4bc33da72902c442df
Nameer Rizvi <rizvinameer@gmail.com>
2020-03-07 21:31:53 -0500

    ext added to proxy

--- src/react/app/proxy.js
+++ src/react/app/proxy.js
@@ -1,8 +1,8 @@
 import { port, endpoint } from "../../shared/index";
 
-export default () => {
+export default ext => {
   const origin = window.location.origin;
   return origin.includes(`:${port.client}`)
-    ? `${origin.replace(port.client, port.server)}${endpoint}`
+    ? `${origin.replace(port.client, port.server)}${endpoint}${ext || ""}`
     : endpoint;
 };
